4260ad94e22878b1944c4eaa1e594cec354ea445
Pablo Julian Jimenez Cano <pajimene@bess-f2r5s8.clusters.42paris.fr>
2025-01-17 11:14:59 +0100

    test

---
+++ srcs/parse.cpp
@@ -0,0 +1,12 @@
+#include "ft_irc.hpp"
+
+
+
+void	Server::parse_args(char *port, char *password)
+{
+	std::string	s_port(port);
+	std::string s_password(password);
+
+	this->port = s_port;
+	this->password = s_password;
+}
